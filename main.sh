c54042d043febfa6ae46132357134aa196916cf4
pleycpl <h4rvey@protonmail.com>
2017-09-13 20:22:58 +0000

    cogu kisim fonksiyonlara ayrildi.

--- main.sh
+++ main.sh
@@ -8,34 +8,64 @@ LINK="https://www.ce.yildiz.edu.tr/personal/furkan"
 FileLink=$LINK/file
 
 
-# Fonksiyonlar
-function learn_extension_in_string() {  # String icinden nokta uzantili uzantiyi cikariyoruz.
+### Fonksiyonlar
+# String icinden nokta uzantili uzantiyi cikariyoruz.
+function learn_extension_in_string() {
+    echo "[+] learn_extension_in_string() fonksiyonu calistirildi."
     local link=$1
     EXTENSION=${link##*.}
 }
-
 # Sitenin kaynak kodunu indiriyoruz. Certificate hatasi aldigimiz icin bu parametreyi kullaniyoruz.
-wget --no-check-certificate $FileLink -O source.html
+function download_source_code() {
+    echo "[+] download_source_code() fonksiyonu calistirildi."
+    local link=$1
+    wget --no-check-certificate $link -O source.html
+}
+
+# Pdf, rar dosyasini indir.
+function download_file() {
+    echo "[+] download_file() fonksiyonu calistirildi."
+    local link=$1
+    wget --no-check-certificate $link
+}
 
 # Kaynak Koddan linkleri cikariyoruz.
-cat source.html | grep -o "$FileLink.*><div" | sed 's/"><div class="iconimage"><\/div><div//' > links.txt
+function parse_link() {
+    echo "[+] parse_link() fonksiyonu calistirildi."
+    local link=$1
+    cat source.html | grep -o "$link.*><div" | sed 's/"><div class="iconimage"><\/div><div//' > links.txt
+}
 
-# Linkleri kontrol ediyoruz. Eger icinde indirilecek uzanti varsa indiriyoruz.
-# Yoksa icindeki diger linklere gidiyoruz. Suanlik ilk sayfadaki dosyayi indiricez.
-for link in `cat links.txt`
-do
-    learn_extension_in_string $link
-    FLAG=0
-    for ext in ${MUST_BE_DOWNLOAD[@]}
+# Gecici dosyalari sil.
+function delete_tmp_file() {
+    rm source.html links.txt 2> /dev/null
+}
+
+# Main kisim.
+function main() {
+    echo "[+] main() fonksiyonu calistirildi."
+    download_source_code $FileLink
+    parse_link $FileLink
+    # Linkleri kontrol ediyoruz. Eger icinde indirilecek uzanti varsa indiriyoruz.
+    # Yoksa icindeki diger linklere gidiyoruz. Suanlik ilk sayfadaki dosyayi indiricez.
+    for link in `cat links.txt`
     do
-        if test "$ext" = $EXTENSION; then
-            FLAG=1
+        learn_extension_in_string $link
+        FLAG=0
+        for ext in ${MUST_BE_DOWNLOAD[@]}
+        do
+            if test "$ext" = $EXTENSION; then
+                FLAG=1
+            fi
+        done
+        if [ "$FLAG" = "1" ]
+        then
+            echo "Tmm indir, panpa"
+            echo $link
+            download_file $link
         fi
     done
-    if [ "$FLAG" = "1" ]
-    then
-        echo "Tmm indir, panpa"
-        echo $link
-        wget --no-check-certificate $link
-    fi
-done
+    delete_tmp_file
+}
+
+main
