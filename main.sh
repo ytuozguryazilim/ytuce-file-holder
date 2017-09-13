cc8065d2395b29c0e60d58044cc8931c2e277b8c
pleycpl <h4rvey@protonmail.com>
2017-09-13 19:56:53 +0000

    Linklerin uzantilari kontrol ediliyor, indirilicek uzantiysa indiriliyor.

--- main.sh
+++ main.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-
 # Global Degiskenler
+FLAG=0
+MUST_BE_DOWNLOAD=("rar" "pdf")
 EXTENSION=""
 LINK="https://www.ce.yildiz.edu.tr/personal/furkan"
 FileLink=$LINK/file
@@ -19,8 +20,22 @@ wget --no-check-certificate $FileLink -O source.html
 # Kaynak Koddan linkleri cikariyoruz.
 cat source.html | grep -o "$FileLink.*><div" | sed 's/"><div class="iconimage"><\/div><div//' > links.txt
 
+# Linkleri kontrol ediyoruz. Eger icinde indirilecek uzanti varsa indiriyoruz.
+# Yoksa icindeki diger linklere gidiyoruz. Suanlik ilk sayfadaki dosyayi indiricez.
 for link in `cat links.txt`
 do
     learn_extension_in_string $link
-    echo $EXTENSION
+    FLAG=0
+    for ext in ${MUST_BE_DOWNLOAD[@]}
+    do
+        if test "$ext" = $EXTENSION; then
+            FLAG=1
+        fi
+    done
+    if [ "$FLAG" = "1" ]
+    then
+        echo "Tmm indir, panpa"
+        echo $link
+        wget --no-check-certificate $link
+    fi
 done
