b7a72a881f4a082942200ca5da075907a492417d
pleycpl <h4rvey@protonmail.com>
2017-09-13 19:38:07 +0000

    learn_extension_in_string fonksiyonu eklendi.

--- main.sh
+++ main.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+
 # Global Degiskenler
+EXTENSION=""
 LINK="https://www.ce.yildiz.edu.tr/personal/furkan"
 FileLink=$LINK/file
+
+
+# Fonksiyonlar
+function learn_extension_in_string() {  # String icinden nokta uzantili uzantiyi cikariyoruz.
+    local link=$1
+    EXTENSION=${link##*.}
+}
 
 # Sitenin kaynak kodunu indiriyoruz. Certificate hatasi aldigimiz icin bu parametreyi kullaniyoruz.
 wget --no-check-certificate $FileLink -O source.html
@@ -10,4 +19,8 @@ wget --no-check-certificate $FileLink -O source.html
 # Kaynak Koddan linkleri cikariyoruz.
 cat source.html | grep -o "$FileLink.*><div" | sed 's/"><div class="iconimage"><\/div><div//' > links.txt
 
-cat links.txt
+for link in `cat links.txt`
+do
+    learn_extension_in_string $link
+    echo $EXTENSION
+done
