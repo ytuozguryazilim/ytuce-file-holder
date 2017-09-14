ecb46c96187178f9701f666828551b03400e7114
pleycpl <h4rvey@protonmail.com>
2017-09-14 16:43:47 +0000

    task listesi README.md ye tasindi.

--- main.sh
+++ main.sh
@@ -85,11 +85,3 @@ function main() {
 }
 
 main
-
-### Task Listesi!!!
-# 1. Kisiler sayfasini crawler edip, buradaki linkleri "personslinks.txt" dosyasina kaydetmeliyiz, ama bazi isimleri engellenmeli("fkord filiz kazim ekoord ...").
-# 2. Dongu kurarak tum kisilerin dosyalari takip edilmeli.
-# 3. "recursive_link_follow()" fonksiyonun icindeki "href" in geri tusu olup olmadigini kontrol etmeliyiz.
-# 4. Karsilastirma yapilmali. Dosya takip sisteminde yok ise indirilmeli. Dosya var ise ayni isimdeki dosyayla karsilastirilmali(Suanlik dosya boyutuna gore). Farkli ise indirilerek ve dosya isminin sonuna tarih atilmali.
-# 5. Bazi hocalar haberler kismina dosyayi koyuyorlar ki her donem sonu silinsin. Ve bir daha ugrasmamak icin. Onun icin haberler kisminida ilerde takip etmeliyiz.
-# 6. Parola konmus dizinlerle ugrasiriz.
